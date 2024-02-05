e35c09318450e57141eec7afad2cd7b9ced2d84d
Pattharanan <s6406021622052@email.kmutnb.ac.th>
2024-02-05 11:02:14 +0700

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
 
-const db = new sqlite3.Database('./Database/Book.sqlite');
+const db = new sqlite3.Database('./Database/SequlizeSQLiteCRUDBook.sqlite');
 
 app.use(express.json());
 
